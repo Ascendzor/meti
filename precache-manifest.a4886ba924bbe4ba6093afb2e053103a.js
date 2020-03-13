e605bfff6f2da20ae744f25d65e8bcef6dc3d445
Ascendzor <wow.troymare@gmail.com>
2020-03-14 11:40:18 +1300

    Updates

---
+++ precache-manifest.a4886ba924bbe4ba6093afb2e053103a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0205dfd65ca046cfcfaa3fe2ec9b2b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "578f3604c5b5e1b63506",
+    "url": "/static/css/main.d93253c0.chunk.css"
+  },
+  {
+    "revision": "7d633d909b9c14dba370",
+    "url": "/static/js/2.1f25bd3d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1f25bd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578f3604c5b5e1b63506",
+    "url": "/static/js/main.d062bef1.chunk.js"
+  },
+  {
+    "revision": "7f277c87c7406db1e994",
+    "url": "/static/js/runtime-main.d7e1f6cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
